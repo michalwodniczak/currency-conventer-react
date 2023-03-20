d392aa43dfa2d33a4aaf92abeb1faeb02059ecf9
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-03-20 18:51:12 +0100

    add content to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <header className="header">
+            <h1 className="header__head">Wymiana walut</h1>
+        </header>
+        <main>
+            <form className="js-form">
+                <label>
+                    <span className="form__labelText">
+                        Wybierz walute na którą chcesz wymienić*:
+                    </span>
+                    <select className="form__input js-select">
+                        <option>USD</option>
+                        <option>EUR</option>
+                        <option>GBP</option>
+                    </select>
+                </label>
+                <label>
+                    <span className="form__labelText">Wpisz ilość w zł*:</span>
+                    <input className="form__input js-amountValue" type="number" name="currrency" step="any" min="1"
+                        required/>
+                </label>
+                <button className="form__button" type="submit">
+                    Przelicz
+                </button>
+                <p className="form__resultText js-finishText">
+                    Tutaj pojawi się kurs walut
+                </p>
+                <p>
+                    Kurs walut z dnia 27.12.2022
+                </p>
+            </form>
+        </main>
+        <footer className="footer">
+            Copyright&copy; 2022 by Widmo
+        </footer>
     </div>
   );
 }
