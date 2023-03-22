59530cd63b8b7c5deeb742877571c61ca25bc035
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-03-22 17:45:04 +0100

    add new component and styles

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import Header from "./Header";
+import {Form} from "./Form";
+
+
 function App() {
   return (
     <div className="container">
         <Header/>
         <main>
-            <form className="js-form">
-                <label>
-                    <span className="form__labelText">
-                        Wybierz walute na którą chcesz wymienić*:
-                    </span>
-                    <select className="form__input js-select">
-                        <option>USD</option>
-                        <option>EUR</option>
-                        <option>GBP</option>
-                    </select>
-                </label>
-                <label>
-                    <span className="form__labelText">Wpisz ilość w zł*:</span>
-                    <input className="form__input js-amountValue" type="number" name="currrency" step="any" min="1"
-                        required/>
-                </label>
-                <button className="form__button" type="submit">
-                    Przelicz
-                </button>
-                <p className="form__resultText js-finishText">
-                    Tutaj pojawi się kurs walut
-                </p>
-                <p>
-                    Kurs walut z dnia 27.12.2022
-                </p>
-            </form>
+            <Form/>
         </main>
         <footer className="footer">
             Copyright&copy; 2022 by Widmo
---
+++ src/Form/index.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./style.css"
+import { currencies } from "../currencies";
+const Form = () => {
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const [amount, setAmount] = useState("");
+
+
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <label>
+                <span className="form__labelText">
+                    Wybierz walute na którą chcesz wymienić*:
+                </span>
+                <select
+                    className="form__input">
+                    {currencies.map(currency => (<option key={currency.id}>{currency.name}</option>)
+                    )}
+                </select>
+            </label>
+            <label>
+                <span className="form__labelText">Wpisz ilość w zł*:</span>
+                <input
+                    value={amount}
+                    onChange={({ target }) => setAmount(target.value)}
+                    className="form__input"
+                    type="number" name="amount"
+                    step="any"
+                    min="1"
+                    required
+                />
+            </label>
+            <button className="form__button" type="submit">
+                Przelicz
+            </button>
+            <p className="form__resultText">
+                Tutaj pojawi się kurs walut
+            </p>
+            <p>
+                Kurs walut z dnia 27.12.2022
+            </p>
+        </form>
+    )
+}
+
+export { Form }
