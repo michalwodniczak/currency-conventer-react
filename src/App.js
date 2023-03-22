f799c6ad7f81d843617b5a3dcb3da9d79f4935b8
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-03-22 17:46:39 +0100

    change default export to name export

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import Header from "./Header";
-import {Form} from "./Form";
+import { Header } from "./Header";
+import { Form } from "./Form";
 
 
 function App() {
-  return (
-    <div className="container">
-        <Header/>
-        <main>
-            <Form/>
-        </main>
-        <footer className="footer">
-            Copyright&copy; 2022 by Widmo
-        </footer>
-    </div>
-  );
+    return (
+        <div className="container">
+            <Header />
+            <main>
+                <Form />
+            </main>
+            <footer className="footer">
+                Copyright&copy; 2022 by Widmo
+            </footer>
+        </div>
+    );
 }
 
 export default App;
--- src/Header/index.js
+++ src/Header/index.js
@@ -6,4 +6,4 @@ const Header = () => (
     </header>
 )
 
-export default Header
+export { Header }
