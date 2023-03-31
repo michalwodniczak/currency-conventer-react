f79c6e3245b64bebd791a3607870dd72aa6ee573
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-03-31 13:21:14 +0200

    add prefix

--- src/currencies.js
+++ src/currencies.js
@@ -2,18 +2,19 @@ const currencies = [
     {
         id: "USD",
         name: "Dolar Amerykański",
-        ratio: 4.35
+        ratio: 4.35,
     },
     {
         id: "EUR",
         name: "Euro",
-        ratio: 4.69
+        ratio: 4.69,
+        forResult: "Euro"
     },
     {
         id: "GBP",
         name: "Funt Brytyjski",
-        ratio: 5.30
+        ratio: 5.30,
     }
 ];
 
-export {currencies}
+export {currencies};
