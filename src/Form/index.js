52246f4758fa5927f13aa4a37af542c1e5850892
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-10-21 13:04:25 +0200

    refactored code and add new components

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,9 @@
-import { useState, useEffect } from "react";
-import { Label, Input, Button, ResultText } from "./styled";
+import { useState } from "react";
+import { Label, Input, Button, ResultText, Failure, Loading } from "./styled";
 import { useGetApiDate } from "./useGetApiDate";
 
 const Form = ({ result, calculateResult }) => {
-    const { date, currencies, error } = useGetApiDate().apiValue;
-    const [render, setRender] = useState(false);
+    const { date, currencies, state } = useGetApiDate().apiValue;
     const [currency, setCurrency] = useState(currencies);
     const [amount, setAmount] = useState("");
     const onFormSubmit = (event) => {
@@ -17,69 +16,67 @@ const Form = ({ result, calculateResult }) => {
         setCurrency(selectCurrency);
     };
 
-    useEffect(() => {
-        setTimeout(() => {
-            setRender(true)
-        }, 1000)
-    }, [])
-
-    if (!render) {
-        return (
-            <p>
-                Trwa pobieranie danych
-            </p>)
-    }
-
-    if (error) {
-        return (
-            <ResultText>{error}</ResultText>
-        )
-    }
-
     return (
         <form onSubmit={onFormSubmit}>
-            <label>
-                <Label>
-                    Wybierz walute na którą chcesz wymienić*:
-                </Label>
-                <Input
-                    as="select"
-                    value={currency ? currency.name : ""}
-                    onChange={onChangeCurrency}
-                >
-                    {currencies.map(currency => (<option key={currency.name} value={currency.name}>{currency.name}</option>)
-                    )};
-                </Input>
-            </label>
-            <label>
-                <Label>
-                    Wpisz ilość w zł*:
-                </Label>
-                <Input
-                    value={amount}
-                    onChange={({ target }) => setAmount(target.value)}
-                    type="number"
-                    name="amount"
-                    step="any"
-                    min="1"
-                    required
-                />
-            </label>
-            <Button
-                onClick={() => calculateResult(amount, currency)}
-                type="submit"
-            >
-                Przelicz
-            </Button>
-            <ResultText>
-                {result
-                    ? `za ${result.sourceAmount.toFixed(2)}zł otrzymamy ${result.targetAmount.toFixed(2)} ${result.currency.name}`
-                    : "Wynik przewalutowania:"
-                }
-            </ResultText>
-            <p>
-                Kursy walut aktualny na dzień: <strong>{date}</strong>
-            </p>
+            {state === "loading" ?
+                (
+                    <Loading>
+                        Sekunda... <br /> ładuje własnie strone poczekaj chwile!
+                    </Loading>
+                )
+                : (
+                    state === "error" ?
+                        (
+                            <Failure>
+                                Wystąpił błąd być może nie masz internetu, bądź nasze żądanie api wyleciało w kosmos :/
+                            </Failure>
+                        ) : (
+                            <>
+                                <label>
+                                    <Label>
+                                        Wybierz walute na którą chcesz wymienić*:
+                                    </Label>
+                                    <Input
+                                        as="select"
+                                        value={currency ? currency.name : ""}
+                                        onChange={onChangeCurrency}
+                                    >
+                                        {currencies.map(currency => (<option key={currency.name} value={currency.name}>{currency.name}</option>)
+                                        )};
+                                    </Input>
+                                </label>
+                                <label>
+                                    <Label>
+                                        Wpisz ilość w zł*:
+                                    </Label>
+                                    <Input
+                                        value={amount}
+                                        onChange={({ target }) => setAmount(target.value)}
+                                        type="number"
+                                        name="amount"
+                                        step="any"
+                                        min="1"
+                                        required
+                                    />
+                                </label>
+                                <Button
+                                    onClick={() => calculateResult(amount, currency)}
+                                    type="submit"
+                                >
+                                    Przelicz
+                                </Button>
+                                <ResultText>
+                                    {result
+                                        ? `za ${result.sourceAmount.toFixed(2)}zł otrzymamy ${result.targetAmount.toFixed(2)} ${result.currency.name}`
+                                        : "Wynik przewalutowania:"
+                                    }
+                                </ResultText>
+                                <p>
+                                    Kursy walut aktualny na dzień: <strong>{date}</strong>
+                                </p>
+                            </>
+                        )
+                )}
         </form >
     )
 }
--- src/Form/useGetApiDate.js
+++ src/Form/useGetApiDate.js
@@ -5,38 +5,38 @@ export const useGetApiDate = () => {
     const [apiValue, setApiValue] = useState({
         date: "",
         currencies: [],
-        error: null
+        state: "loading"
     });
-    
-    const url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_NdeppBDyPclNp621W5p3YmiJkDVgwAQWjWOUIp0N&currencies=EUR%2CUSD%2CGBP&base_currency=PLN";
 
-    const getDate = async () => {
-        
-        try {
-            const response = await axios.get(url);
-            const apiDate = response.data.meta.last_updated_at;
-            const myDate = new Date(apiDate);
-            const date = myDate.toLocaleDateString();
-            const currenciesKey = Object.keys(response.data.data);
-            const currencyData = currenciesKey.map(currency => ({
-                name: currency,
-                value: response.data.data[currency]?.value
-            }))
-            setApiValue({
-                date: date,
-                currencies: currencyData,
-                error: null
-            });
-            if (!response.ok) {
-                new Error(response.statusText);
-            };
-        }
-        catch (error) {
-            setApiValue({ error: "Wystąpił błąd, coś poszło nie tak:(. Sprawdź swoje połączenie z internetem. Jeśli masz to wygląda na to że to nasza wina :<. Spróbuj ponownie za jakiś czas" });
-        }
-    };
     useEffect(() => {
-        getDate();
-    },[]);
-    return { apiValue }
+        const url = "ddhttps://api.currencyapi.com/v3/latest?apikey=cur_live_NdeppBDyPclNp621W5p3YmiJkDVgwAQWjWOUIp0N&currencies=EUR%2CUSD%2CGBP&base_currency=PLN";
+
+        const getDate = async () => {
+
+            try {
+                const response = await axios.get(url);
+                const apiDate = response.data.meta.last_updated_at;
+                const myDate = new Date(apiDate);
+                const date = myDate.toLocaleDateString();
+                const currenciesKey = Object.keys(response.data.data);
+                const currencyData = currenciesKey.map(currency => ({
+                    name: currency,
+                    value: response.data.data[currency]?.value
+                }))
+                setApiValue({
+                    state: "success",
+                    date: date,
+                    currencies: currencyData,
+                });
+                if (!response.ok) {
+                    new Error(response.statusText);
+                };
+            }
+            catch {
+                setApiValue({ state: "error" });
+            }
+        };
+        setTimeout(getDate, 1000);
+    }, []);
+    return { apiValue };
 };
