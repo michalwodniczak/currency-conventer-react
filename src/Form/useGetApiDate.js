f20cc0aa21028c2acd3ed00b9b78d78ffca716df
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-11-06 18:24:36 +0100

    changed url adress and refactored code

--- src/Form/useGetApiDate.js
+++ src/Form/useGetApiDate.js
@@ -3,18 +3,22 @@ import { useState, useEffect } from "react";
 
 export const useGetApiDate = () => {
     const [apiValue, setApiValue] = useState({
+        state: "loading",
         date: "",
-        currencies: [],
-        state: "loading"
+        currencies:{},
     });
 
     useEffect(() => {
-        const url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_NdeppBDyPclNp621W5p3YmiJkDVgwAQWjWOUIp0N&currencies=EUR%2CUSD%2CGBP&base_currency=PLN";
-
+        const url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_OXoP0m9yL8nJ1iezafrmUwJssz7HSFb1rfAaGCDx&currencies=EUR%2CUSD%2CGBP&base_currency=PLN";
+        
         const getDate = async () => {
-
             try {
                 const response = await axios.get(url);
+
+                if (!response.ok) {
+                    new Error(response.statusText);
+                };
+
                 const apiDate = response.data.meta.last_updated_at;
                 const myDate = new Date(apiDate);
                 const date = myDate.toLocaleDateString();
@@ -23,14 +27,13 @@ export const useGetApiDate = () => {
                     name: currency,
                     value: response.data.data[currency]?.value
                 }))
+
                 setApiValue({
                     state: "success",
                     date: date,
                     currencies: currencyData,
                 });
-                if (!response.ok) {
-                    new Error(response.statusText);
-                };
+
             }
             catch {
                 setApiValue({ state: "error" });
